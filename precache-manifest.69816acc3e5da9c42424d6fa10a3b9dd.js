e35af40e9bb79a2c71a5720e55cdce11caef8d74
Krutik <krutikshah6@gmail.com>
2020-07-10 02:39:11 +0530

    Updates

---
+++ precache-manifest.69816acc3e5da9c42424d6fa10a3b9dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bc65fd68a616696bb1cb3c85f992a1",
+    "url": "/MoviesInfo/index.html"
+  },
+  {
+    "revision": "33f7fd84b53b886ba1df",
+    "url": "/MoviesInfo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "78b0bd7cc6fc24f4594f",
+    "url": "/MoviesInfo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33f7fd84b53b886ba1df",
+    "url": "/MoviesInfo/static/js/2.bd770960.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MoviesInfo/static/js/2.bd770960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b0bd7cc6fc24f4594f",
+    "url": "/MoviesInfo/static/js/main.9e3faee0.chunk.js"
+  },
+  {
+    "revision": "652a26b936e58bf20b03",
+    "url": "/MoviesInfo/static/js/runtime-main.43e48149.js"
+  }
+]);
