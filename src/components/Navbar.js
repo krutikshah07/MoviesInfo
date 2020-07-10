e76a7f75e20f52736a695d0c3f1cd4ade735a450
Krutik <krutikshah6@gmail.com>
2020-07-10 16:35:06 +0530

    Navbar fiz

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class Navbar1 extends Component{
- 
-
-render(){
-    return(
-        <Navbar dark color="dark">
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-dark mb-5">
         <div className="container">
-          <NavbarBrand href="/">Movies Information</NavbarBrand>
+          <div className="navbar-header">
+            <Link className="navbar-brand text-white text-lg brand-text" to="/">
+              MovieSeriesInfo
+            </Link>
+          </div>
+          <ul className="navbar-nav ml-auto text-light d-inline-block">
+            <li className="nav-item d-inline-block mr-4">
+             
+            </li>
+            <li className="nav-item d-inline-block mr-4">
+              
+            </li>
+          </ul>
         </div>
-      </Navbar>
-     
-    );
-    
-}
+      </nav>
+    </div>
+  );
 }
 
-
-export default Navbar1;
+export default Navbar;
